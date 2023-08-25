27630459bbe444e7bf995b59670282d4c5375087
canadrian72 <patterson.n.adrian@gmail.com>
2023-08-25 17:47:57 -0400

    Fixing browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { darkModeAtom, theme } from "./Utils/Theme";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import { useAtom } from "jotai";
 import { Grommet } from "grommet";
 import Hikes from "./Pages/Hikes";
 
 function App(): JSX.Element {
-    const [darkMode] = useAtom(darkModeAtom);
+  const [darkMode] = useAtom(darkModeAtom);
 
-    return (
-        <Grommet theme={theme} full themeMode={darkMode ? "dark" : "light"}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/hikes" element={<Hikes />} />
-                </Routes>
-            </BrowserRouter>
-        </Grommet>
-    );
+  return (
+    <Grommet theme={theme} full themeMode={darkMode ? "dark" : "light"}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hikes" element={<Hikes />} />
+        </Routes>
+      </HashRouter>
+    </Grommet>
+  );
 }
 
 export default App;
