0cd86d754e9884d0b9972f4cf486aa9b7bab63bf
Adrian Patterson <patterson.n.adrian@gmail.com>
2024-02-12 13:08:17 -0500

    Using code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { darkModeAtom, theme } from "./Utils/Theme";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./Pages/Home/HomePage";
 import { useAtom } from "jotai";
 import { Grommet } from "grommet";
-import HikesPage from "./Pages/Hikes/HikesPage";
-import AlgonquinPeak from "./Pages/BlogPages/AlgonquinPeak/AlgonquinPeak";
-import ThreeDimensionalMap from "./Pages/ThreeDimensionalMap/ThreeDimensionalMap";
 import { BlogPath } from "./Enums/blogPath";
-import PathOfTheGods from "./Pages/BlogPages/PathOfTheGods/PathOfTheGods";
-import PathTheLemons from "./Pages/BlogPages/PathOfTheLemons/PathOfLemons";
+import { Suspense, lazy } from "react";
+
+const HomePage = lazy(() => import("./Pages/Home/HomePage"));
+const HikesPage = lazy(() => import("./Pages/Hikes/HikesPage"));
+const ThreeDimensionalMap = lazy(
+  () => import("./Pages/ThreeDimensionalMap/ThreeDimensionalMap")
+);
+const PathOfTheGods = lazy(
+  () => import("./Pages/BlogPages/PathOfTheGods/PathOfTheGods")
+);
+const PathOfLemons = lazy(
+  () => import("./Pages/BlogPages/PathOfTheLemons/PathOfLemons")
+);
+const AlgonquinPeak = lazy(
+  () => import("./Pages/BlogPages/AlgonquinPeak/AlgonquinPeak")
+);
 
 function App(): JSX.Element {
   const [darkMode] = useAtom(darkModeAtom);
@@ -16,14 +26,16 @@ function App(): JSX.Element {
   return (
     <Grommet theme={theme} full themeMode={darkMode ? "dark" : "light"}>
       <HashRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/hikes" element={<HikesPage />} />
-          <Route path="/3d" element={<ThreeDimensionalMap />} />
-          <Route path={BlogPath.AlgonquinPeak} element={<AlgonquinPeak />} />
-          <Route path={BlogPath.PathOfTheGods} element={<PathOfTheGods />} />
-          <Route path={BlogPath.PathOfTheLemons} element={<PathTheLemons />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/hikes" element={<HikesPage />} />
+            <Route path="/3d" element={<ThreeDimensionalMap />} />
+            <Route path={BlogPath.AlgonquinPeak} element={<AlgonquinPeak />} />
+            <Route path={BlogPath.PathOfTheGods} element={<PathOfTheGods />} />
+            <Route path={BlogPath.PathOfTheLemons} element={<PathOfLemons />} />
+          </Routes>
+        </Suspense>
       </HashRouter>
     </Grommet>
   );
